16b2a64bfbee48a8f4219c703c8ad272e9192764
Madhu-712 <88141363+Madhu-712@users.noreply.github.com>
2025-01-30 19:09:57 +0530

    Update Tutor.py

--- Tutor.py
+++ Tutor.py
@@ -13,7 +13,7 @@ from tempfile import NamedTemporaryFile
 
 # Define system prompt and instructions
 
-SYSTEM_PROMPTS="""You are an AI Tutor specializing in generating concise explanations and real-world examples for educational topics. Your role is to analyze provided chapter titles, deliver brief and intuitive explanations for each, offer relevant real-world examples, and generate 20 frequently asked questions (FAQs) with answers for each topic. Present all information in a clear, organized, and student-friendly manner.Pls use Markdown format"""
+SYSTEM_PROMPTS="""You are an AI Tutor specializing in generating concise explanations and real-world examples for educational topics. Your role is to analyze provided chapter titles or queries, deliver brief and intuitive explanations for each, offer relevant real-world examples, and generate 20 frequently asked questions (FAQs) with answers for each topic. Present all information in a clear, organized, and student-friendly manner.Pls use Markdown format"""
 
 INSTRUCTIONS ="""
 * For each provided chapter title:
@@ -23,6 +23,7 @@ INSTRUCTIONS ="""
         - Offer relevant real-world examples that illustrate the topic, enhancing understanding and applicability.
     3. **Frequently Asked Questions (FAQs)**:
         - Generate a list of 20 pertinent FAQs related to the each topic.
+        - For Math related topic ensure the queries are more of problems and solve step wise .
         - Provide clear and accurate answers to each question.
 * Ensure that all step by step explanations, examples, and FAQs are tailored to facilitate student comprehension and engagement.
 * Present the information in a structured format, using headings, bullet points, and numbering to enhance readability.
@@ -79,7 +80,7 @@ def main():
     st.write("Upload an image containing chapter names to receive explanations, real-world examples, and FAQs for each topic.")
 
     uploaded_file = st.file_uploader(
-        "Upload an image with chapter names", 
+        "Upload an image with 'chapter names' or 'queries', 
         type=["jpg", "jpeg", "png"],
         help="Ensure the image is clear and the text is legible."
     )
