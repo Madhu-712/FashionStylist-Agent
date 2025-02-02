cec8733a40ce2918f96d78f64382e3ff1207a5dc
Madhu-712 <88141363+Madhu-712@users.noreply.github.com>
2025-02-02 20:23:40 +0530

    Update Google.py

--- Google.py
+++ Google.py
@@ -7,10 +7,11 @@ from phi.tools.tavily import TavilyTools
 
 # Define system prompt and instructions
 
-SYSTEM_PROMPTS="""**You are a google search gpt agent that helps users search the information from the web and give response in markdown format.**"""
-INSTRUCTIONS=""" **Search a topic given by the user, respond with 4 latest news items about that topic.
-**Cite source of the response.
-**Give alternate web links,book recommendations ,resources etc for more in depth nuances on the topic."""
+SYSTEM_PROMPTS="""*You are a google search gpt agent that helps users search the information from the web and give response in markdown format."""
+INSTRUCTIONS=""" 
+*Search a topic given by the user, respond with 4 latest news items about that topic.
+*Cite source of the response.
+*Give alternate web links,book recommendations ,resources etc for more in depth nuances on the topic."""
 
         
 # Set API keys from Streamlit secrets
